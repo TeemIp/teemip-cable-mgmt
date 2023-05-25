82e3a775e3d8a53abdcd93d3af537abe12bee2a1
xtophe38 <christophe.naud@gmail.com>
2023-05-25 11:25:34 +0200

    Add bridge for DC view extended

---
+++ teemip-bridge-datacenter-view-extended-cable/dictionaries/en.dict.teemip-bridge-datacenter-view-extended-cable.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2023 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// Class: PatchPanel
+//
+
+Dict::Add('EN US', 'English', 'English', array(
+	'Class:PatchPanel/Attribute:nb_u' => 'Height',
+	'Class:PatchPanel/Attribute:nb_u+' => 'Height in units (U)',
+	'Class:PatchPanel/Attribute:position_p' => 'Panel',
+	'Class:PatchPanel/Attribute:position_p+' => 'Is the patch panel mounted on the front or rear panel of the rack ?',
+	'Class:PatchPanel/Attribute:position_p/Value:front' => 'Front',
+	'Class:PatchPanel/Attribute:position_p/Value:rear' => 'Rear',
+	'Class:PatchPanel/Attribute:position_v' => 'Vert. position',
+	'Class:PatchPanel/Attribute:position_v+' => 'Vertical position (U) of the patch panel in the rack. (Must be the bottom position, not top)',
+	'Class:PatchPanel/Attribute:weight' => 'Weight (kg)',
+	'Class:PatchPanel/Attribute:weight+' => 'Weight of the patch panel',
+	'Class:PatchPanel/Attribute:expected_power_input' => 'Expected consumption (W)',
+	'Class:PatchPanel/Attribute:expected_power_input+' => 'Expected power consumption of the patch panel',
+	'Class:PatchPanel/Attribute:zero_u' => 'Zero-U',
+	'Class:PatchPanel/Attribute:zero_u+' => 'Only set to \'Yes\' if the patch panel does not fit in standard racks layout',
+	'Class:PatchPanel/Attribute:zero_u/Value:yes' => 'Yes',
+	'Class:PatchPanel/Attribute:zero_u/Value:no' => 'No',
+));
+
+
+
---
+++ teemip-bridge-datacenter-view-extended-cable/module.teemip-bridge-datacenter-view-extended-cable.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2022 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'teemip-bridge-datacenter-view-extended-cable/1.1.0',
+	array(
+		// Identification
+		//
+		'label' => 'Links between Molkobain Datacenter view extended and cable management',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'teemip-cable-mgmt/1.1.0',
+			'molkobain-datacenter-view-extended/1.8.2||teemip-cable-mgmt/1.1.0',
+		),
+		'mandatory' => false,
+		'visible' => false,
+		'auto_select' => 'SetupInfo::ModuleIsSelected("teemip-cable-mgmt") && SetupInfo::ModuleIsSelected("molkobain-datacenter-view")',
+
+		// Components
+		//
+		'datamodel' => array(
+		),
+		'data.struct' => array(
+		),
+		'data.sample' => array(
+		),
+
+		// Documentation
+		//
+		'doc.manual_setup' => '',
+		'doc.more_information' => '',
+
+		// Default settings
+		//
+		'settings' => array(),
+	)
+);
+
