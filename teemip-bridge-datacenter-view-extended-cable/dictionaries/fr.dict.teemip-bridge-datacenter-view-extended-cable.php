b90e418cc5905ab2e19415f0d044b5eb77a54c8c
xtophe38 <christophe.naud@gmail.com>
2023-06-28 13:57:08 +0200

    Create fr translation

---
+++ teemip-bridge-datacenter-view-extended-cable/dictionaries/fr.dict.teemip-bridge-datacenter-view-extended-cable.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2023 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// Class: PatchPanel
+//
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Class:PatchPanel/Attribute:nb_u' => 'Hauteur',
+	'Class:PatchPanel/Attribute:nb_u+' => 'Hauteur en unités (U)',
+	'Class:PatchPanel/Attribute:position_p' => 'Face',
+	'Class:PatchPanel/Attribute:position_p+' => 'Face du rack sur laquelle le panneau est positionné',
+	'Class:PatchPanel/Attribute:position_p/Value:front' => 'Avant',
+	'Class:PatchPanel/Attribute:position_p/Value:rear' => 'Arrière',
+	'Class:PatchPanel/Attribute:position_v' => 'Position vert.',
+	'Class:PatchPanel/Attribute:position_v+' => 'Position verticale (U) dans le rack. Doit faire référence au bas et non au haut.',
+	'Class:PatchPanel/Attribute:weight' => 'Poids (kg)',
+	'Class:PatchPanel/Attribute:weight+' => 'Poids du panneau',
+	'Class:PatchPanel/Attribute:expected_power_input' => 'Consommation attendue (W)',
+	'Class:PatchPanel/Attribute:expected_power_input+' => 'Consommation attendue du panneau de brassage',
+	'Class:PatchPanel/Attribute:zero_u' => 'Zéro U',
+	'Class:PatchPanel/Attribute:zero_u+' => 'Mettre à \'Oui\' uniquement si le panneau ne rentre pas dans un emplacement de rack standard',
+	'Class:PatchPanel/Attribute:zero_u/Value:yes' => 'Oui',
+	'Class:PatchPanel/Attribute:zero_u/Value:no' => 'Non',
+));
+
+
+
