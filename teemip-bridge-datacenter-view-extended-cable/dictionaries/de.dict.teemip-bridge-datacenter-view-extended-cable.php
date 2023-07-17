a0ffe0aae97e49ee20ec00145a08d6da61991538
xtophe38 <christophe.naud@gmail.com>
2023-07-17 18:19:36 +0200

    Create dictionary

---
+++ teemip-bridge-datacenter-view-extended-cable/dictionaries/de.dict.teemip-bridge-datacenter-view-extended-cable.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2023 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+//
+// Class: PatchPanel
+//
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+	'Class:PatchPanel/Attribute:nb_u' => 'Höhe',
+	'Class:PatchPanel/Attribute:nb_u+' => 'Höhe in Höheneinheiten (U)',
+	'Class:PatchPanel/Attribute:position_p' => 'Panel',
+	'Class:PatchPanel/Attribute:position_p+' => 'Ist das Patch Panel auf der Vorder- oder auf der Rückseite des Racks montiert?',
+	'Class:PatchPanel/Attribute:position_p/Value:front' => 'Vorderseite',
+	'Class:PatchPanel/Attribute:position_p/Value:rear' => 'Rückseite',
+	'Class:PatchPanel/Attribute:position_v' => 'Vert. Position',
+	'Class:PatchPanel/Attribute:position_v+' => 'Vertikale Position (U) des Patch Panels im Rack (Muss die unterste Position sein, nicht die obere)',
+	'Class:PatchPanel/Attribute:weight' => 'Gewicht (kg)',
+	'Class:PatchPanel/Attribute:weight+' => 'Gewicht des Patch Panel',
+	'Class:PatchPanel/Attribute:expected_power_input' => 'Erwarteter Stromverbrauch (W)',
+	'Class:PatchPanel/Attribute:expected_power_input+' => 'Erwarteter Stromverbrauch des Patch Panel',
+	'Class:PatchPanel/Attribute:zero_u' => 'Null-U',
+	'Class:PatchPanel/Attribute:zero_u+' => 'Nur auf \'Ja\' setzen, wenn das Patch Panel nicht in das Standard Rack Layout passt',
+	'Class:PatchPanel/Attribute:zero_u/Value:yes' => 'Ja',
+	'Class:PatchPanel/Attribute:zero_u/Value:no' => 'Nein',
+));
