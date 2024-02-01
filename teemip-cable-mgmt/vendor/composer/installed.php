b7b6259fc670d2902bdeafec00f41e48fe6956af
xtophe38 <christophe.naud@gmail.com>
2024-02-01 10:55:49 +0100

    Install composer for the extension

---
+++ teemip-cable-mgmt/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ff547a11b51042b45cb5b6c9ddbaa63786cbe850',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ff547a11b51042b45cb5b6c9ddbaa63786cbe850',
+            'dev_requirement' => false,
+        ),
+    ),
+);
