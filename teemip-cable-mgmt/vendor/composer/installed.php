6523f34d707c5e1bbe50a905a05ea7405ae8f0aa
xtophe38 <christophe.naud@gmail.com>
2024-06-04 16:51:13 +0200

    Update composer

--- teemip-cable-mgmt/vendor/composer/installed.php
+++ teemip-cable-mgmt/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
+        'reference' => 'd4b23112b2c06f935f2d91c80c2bf224b59db6af',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'ff547a11b51042b45cb5b6c9ddbaa63786cbe850',
-        'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
+            'reference' => 'd4b23112b2c06f935f2d91c80c2bf224b59db6af',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'ff547a11b51042b45cb5b6c9ddbaa63786cbe850',
             'dev_requirement' => false,
         ),
     ),
